0e443af35f5c488f12089f863f2f1e59f1d330f2
Jari Timonen <jari.timonen@codento.com>
2022-04-06 06:27:35 +0300

    add github back

--- gatsby-config.js
+++ gatsby-config.js
@@ -128,11 +128,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    {
-      resolve: `gatsby-plugin-netlify-cms`,
-      options: {
-        enableIdentityWidget: false,
-      }
-    },
+    `gatsby-plugin-netlify-cms`,
   ],
 }
